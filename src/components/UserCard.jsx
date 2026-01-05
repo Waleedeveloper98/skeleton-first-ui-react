d557b48354b3f5304d346e13f7e72e69e41d7100
Waleed <waleedeveloper100@gmail.com>
2026-01-05 16:34:58 +0500

    responsive fully completed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const UserCard = ({ user }) => {
   const { name, email, avatar, role } = user;
   return (
-    <div className="group bg-[#212123] rounded-2xl p-3 px-4 py-5 w-120 max-w-120 flex gap-5 items-center hover:bg-[#36363A] transition-all duration-300 cursor-pointer">
+    <div className="group bg-[#212123] rounded-2xl p-3 px-4 py-5 w-90 max-w-90 sm:w-120 sm:max-w-120 flex gap-5 items-center hover:bg-[#36363A] transition-all duration-300 cursor-pointer">
       {/* user image */}
-      <div className=" w-1/5 overflow-hidden">
+      <div className=" w-1/5 overflow-hidden hidden sm:block">
         <img
           className="size-12 border-[1.35px] border-white/10 rounded-full object-contain"
           src={avatar || "https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
--- src/components/UserCardSkeleton.jsx
+++ src/components/UserCardSkeleton.jsx
@@ -3,9 +3,9 @@ import Skeleton from "./Skeleton";
 
 const UserCardSkeleton = () => {
   return (
-    <div className="group bg-[#212123] rounded-2xl p-3 px-4 py-5 w-120 max-w-120 flex gap-5 items-center hover:bg-[#36363A] transition-all duration-300 cursor-pointer">
+    <div className="group bg-[#212123] rounded-2xl p-3 px-4 py-5 w-90 max-w-90 sm:w-120 sm:max-w-120 flex gap-5 items-center hover:bg-[#36363A] transition-all duration-300 cursor-pointer">
       {/* user image */}
-      <Skeleton className={"rounded-full size-12"} />
+      <Skeleton className={"rounded-full size-12 hidden sm:block"} />
 
       {/* userdetails */}
       <div className="details flex flex-col gap-1.5">
@@ -15,11 +15,9 @@ const UserCardSkeleton = () => {
 
       {/* button and status */}
       <div className="extra-details flex items-center gap-2 text-xs whitespace-nowrap">
-
-        <Skeleton className={"w-18 h-6.5 rounded-full"} />
-        <Skeleton className={"w-24 h-7 rounded-full"} />
+        <Skeleton className={"w-16 sm:w-18 h-6.5 rounded-full"} />
+        <Skeleton className={"w-20 sm:w-24 h-7 rounded-full"} />
       </div>
-
     </div>
   );
 };
